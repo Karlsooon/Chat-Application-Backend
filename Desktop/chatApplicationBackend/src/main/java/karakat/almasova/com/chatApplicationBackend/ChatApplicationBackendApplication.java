b8cc9117d777a8728f825a5b7b51f23e62d6e8e9
Karlsooon <karakatalmasova2@gmail.com>
2024-03-18 01:45:53 +0500

    first commit

---
+++ Desktop/chatApplicationBackend/src/main/java/karakat/almasova/com/chatApplicationBackend/ChatApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package karakat.almasova.com.chatApplicationBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplicationBackendApplication.class, args);
+	}
+
+}
