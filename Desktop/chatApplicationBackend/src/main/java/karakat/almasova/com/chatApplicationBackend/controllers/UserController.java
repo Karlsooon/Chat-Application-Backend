587d185af39a7e36c4a020a9824a7497743a9777
Karlsooon <karakatalmasova2@gmail.com>
2024-03-19 14:34:50 +0500

    pom.xml

--- Desktop/chatApplicationBackend/src/main/java/karakat/almasova/com/chatApplicationBackend/controllers/UserController.java
+++ Desktop/chatApplicationBackend/src/main/java/karakat/almasova/com/chatApplicationBackend/controllers/UserController.java
@@ -3,6 +3,6 @@ package karakat.almasova.com.chatApplicationBackend.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class UserController {
 }
