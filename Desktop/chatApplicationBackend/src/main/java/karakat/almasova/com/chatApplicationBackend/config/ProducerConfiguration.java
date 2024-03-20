d2897c975c608458a542ecc40965613379510219
Karlsooon <karakatalmasova2@gmail.com>
2024-03-20 17:39:30 +0500

    producer config update

--- Desktop/chatApplicationBackend/src/main/java/karakat/almasova/com/chatApplicationBackend/config/ProducerConfiguration.java
+++ Desktop/chatApplicationBackend/src/main/java/karakat/almasova/com/chatApplicationBackend/config/ProducerConfiguration.java
@@ -2,16 +2,17 @@ package karakat.almasova.com.chatApplicationBackend.config;
 
 import karakat.almasova.com.chatApplicationBackend.models.Message;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.stereotype.Controller;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 @EnableKafka
 @Configuration
@@ -25,6 +26,14 @@ public class ProducerConfiguration {
     public Map<String, Object> producerConfigurations(){
         Map<String, Object> configurations = new HashMap<>();
         configurations.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configurations.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configurations.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return configurations;
 
+    }
+
+    @Bean
+    public KafkaTemplate<String, Message> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
