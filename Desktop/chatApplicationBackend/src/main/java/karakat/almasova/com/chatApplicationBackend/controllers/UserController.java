edcab6052e2cf6c39894e0359292487c6ff61d68
Karlsooon <karakatalmasova2@gmail.com>
2024-03-20 17:41:09 +0500

    controller

--- Desktop/chatApplicationBackend/src/main/java/karakat/almasova/com/chatApplicationBackend/controllers/UserController.java
+++
@@ -1,8 +0,0 @@
-package karakat.almasova.com.chatApplicationBackend.controllers;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UserController {
-
-}
