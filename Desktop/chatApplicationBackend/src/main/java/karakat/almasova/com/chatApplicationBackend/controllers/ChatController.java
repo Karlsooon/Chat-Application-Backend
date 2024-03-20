9a964a0a44ccb73390bae818308569b2047fd9ab
Karlsooon <karakatalmasova2@gmail.com>
2024-03-20 17:46:16 +0500

    controller kafka template

--- Desktop/chatApplicationBackend/src/main/java/karakat/almasova/com/chatApplicationBackend/controllers/ChatController.java
+++ Desktop/chatApplicationBackend/src/main/java/karakat/almasova/com/chatApplicationBackend/controllers/ChatController.java
@@ -1,4 +1,13 @@
 package karakat.almasova.com.chatApplicationBackend.controllers;
 
+import karakat.almasova.com.chatApplicationBackend.models.Message;
+import lombok.NoArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@NoArgsConstructor
 public class ChatController {
+    private KafkaTemplate<String, Message> kafkaTemplate;
+
 }
