70768f9d13d7a67186c928e0548c9e3063e388ff
Karlsooon <karakatalmasova2@gmail.com>
2024-03-22 00:54:38 +0500

    delete user model

--- Desktop/chatApplicationBackend/src/main/java/karakat/almasova/com/chatApplicationBackend/models/User.java
+++
@@ -1,8 +0,0 @@
-package karakat.almasova.com.chatApplicationBackend.models;
-
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-
-public class User {
-    private String username;
-}
