bbf70d7426498f591486287e673aab041cfb7a79
Karlsooon <karakatalmasova2@gmail.com>
2024-03-23 17:02:15 +0500

    chat room model

---
+++ Desktop/chatApplicationBackend/src/main/java/karakat/almasova/com/chatApplicationBackend/models/ChatRoom.java
@@ -0,0 +1,8 @@
+package karakat.almasova.com.chatApplicationBackend.models;
+
+public class ChatRoom {
+    private String id;
+    private String chatId;
+    private String senderId;
+    private String recipientId;
+}
